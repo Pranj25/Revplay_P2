47a2e20834549ac3f4a350c016d90d79ded2601e
Pranjal palpattuwar <154740497+Pranj25@users.noreply.github.com>
2026-03-16 22:39:01 +0530

    Delete backend directory

--- backend/src/main/java/com/revplay/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.revplay.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
